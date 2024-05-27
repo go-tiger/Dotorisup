44a23f4c65805376721be0af9ac2714d33c357bc
go-tiger <dev.gotiger@gmail.com>
2024-05-28 02:34:10 +0900

    feat: 0.0.4 Release - distro url

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager');
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://gotiger.ipdisk.co.kr:99/publist/HDD1/Launcher/Dotorisup/distribution.json';
+exports.REMOTE_DISTRO_URL = 'https://github.com/go-tiger/Launcher-distro-file/raw/main/Dotorisup/distribution.json';
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
